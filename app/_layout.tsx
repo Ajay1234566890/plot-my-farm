7a46b7bba8b74f5696420dcca1c96a8c6be14ee1
rajat693 <rajatchaudhary.rc693@gmail.com>
2025-10-14 13:59:44 +0530

    initial commit

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -6,12 +6,9 @@ import {
 import { Stack } from "expo-router";
 import { StatusBar } from "expo-status-bar";
 import "react-native-reanimated";
-import "../global.css";
-
 import { useColorScheme } from "@/hooks/use-color-scheme";
-
-import { GluestackUIProvider } from '@/components/ui/gluestack-ui-provider';
-import '@/global.css';
+import { GluestackUIProvider } from "@/components/ui/gluestack-ui-provider";
+import "@/global.css";
 
 export const unstable_settings = {
   anchor: "(tabs)",
@@ -21,19 +18,17 @@ export default function RootLayout() {
   const colorScheme = useColorScheme();
 
   return (
-    
     <GluestackUIProvider mode="dark">
       <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen
-          name="modal"
-          options={{ presentation: "modal", title: "Modal" }}
-        />
-      </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
+        <Stack>
+          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+          <Stack.Screen
+            name="modal"
+            options={{ presentation: "modal", title: "Modal" }}
+          />
+        </Stack>
+        <StatusBar style="auto" />
+      </ThemeProvider>
     </GluestackUIProvider>
-  
   );
 }
